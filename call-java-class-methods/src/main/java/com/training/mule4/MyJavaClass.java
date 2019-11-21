d359a0cb13a2ee34959b30186f46ad9be6a59d5c
Amit Kumar Agrawal <amitkumar.agrawal@publicissapient.com>
2019-11-21 10:20:50 +0530

    udemy solution

---
+++ call-java-class-methods/src/main/java/com/training/mule4/MyJavaClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.training.mule4;
+
+/**
+ * @author amiagraw1
+ *
+ */
+public class MyJavaClass {
+	
+	public static String sayHelloToMule(String name) {
+		return "Hello "+name+" ,Welcome to mule world ";
+	}
+	
+	public String sayHelloToMule1(String name) {
+		return "Hello "+name+" ,Welcome to mule instance world ";
+	}
+
+}
